4fea38745004c8125c278ec60f40bd2b00d2e5be
AnotherDev <joseluismanco37@gmail.com>
2024-07-08 20:06:19 +0300

    feat: implement steal

--- ZKProofs/liquidity-lock-proof/methods/guest/src/main.rs
+++ ZKProofs/liquidity-lock-proof/methods/guest/src/main.rs
@@ -1,9 +1,20 @@
-use core::{LiquidityLookResult, Stake};
+use proof_core::core::{LiquidityLookResult, Stake};
 use risc0_zkvm::{
     guest::env,
     serde::to_vec,
     sha::{Impl, Sha256},
 };
+use alloy_sol_types::{sol, SolValue};
+
+
+
+sol! {
+    interface IERC20 {
+        function balanceOf(address account) external view returns (uint);
+    }
+}
+
+
 
 fn main() {
     let params: Stake = env::read();
