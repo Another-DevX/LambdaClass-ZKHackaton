88d9269dec72900f6dfa817189c198cb562ed0d8
ArturVargas <arturvargas9226@gmail.com>
2024-07-08 05:46:51 -0600

    add deposit contract

---
+++ YTP-Contracts/contracts/Lock.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CollateralDeposit {
+
+    address constant collateralToken = 0xa0289cBbEB673b8787C9C61Bf03914068A033651;
+    mapping(address => uint256) collateralValue;
+
+    event CollateralDeposit(
+        address indexed sender, 
+        uint256 amount, 
+        uint256 totalOfCollateralDeposited
+    );
+
+    event Withdrawal(
+        address indexed sender, 
+        uint256 amount, 
+        uint256 totalOfCollateralDeposited
+    );
+
+    function depositCollateral(uint256 _amount)
+        external
+    {
+
+        if (_amount == 0) {
+            revert();
+        }
+        IERC20(collateralToken).transferFrom(msg.sender, address(this), _amount);
+
+        collateralValue[msg.sender] += _amount;
+        emit CollateralDeposit(msg.sender, _amount, collateralValue[msg.sender]);
+    }
+
+    function withdraw() external {
+        
+    }
+}
